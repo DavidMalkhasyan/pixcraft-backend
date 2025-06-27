db7c3d23f456628a2b1020218d60d246a0cbd2fc
David Malkhasyan <malkhasyandavid2@gmail.com>
2025-06-27 16:16:32 +0400

    update mongo connect

--- app.js
+++ app.js
@@ -4,19 +4,25 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
 import photoRoutes from "./routes/photoRoutes.js";
+import dotenv from 'dotenv';
 
+dotenv.config();
+const mongoUri = process.env.MONGO_URI;
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const mongoUri = process.env.MONGO_URI;
 if (!mongoUri) {
   console.error("MongoDB URI is not defined in environment variables");
   process.exit(1);
 }
 
-mongoose.connect("mongoUri")
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.error("MongoDB error", err));
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log('MongoDB connected'))
+.catch((err) => console.error('MongoDB connection error:', err));
 
 app.use(cors({
   origin: "http://localhost:3000",
