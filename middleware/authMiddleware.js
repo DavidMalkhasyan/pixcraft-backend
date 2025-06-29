1e7159e8fb9f8ef87b1ce36cdcdd3aa862f0618d
David Malkhasyan <malkhasyandavid2@gmail.com>
2025-06-29 15:14:22 +0400

    change middlwear

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -2,14 +2,19 @@ import jwt from "jsonwebtoken";
 const JWT_SECRET = "mySecret123";
 
 export const authMiddleware = (req, res, next) => {
-  const token = req.cookies.token;
-  if (!token) return res.status(401).json({ error: "No token" });
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({ error: "No token provided" });
+  }
+
+  const token = authHeader.split(" ")[1];
 
   try {
     const decoded = jwt.verify(token, JWT_SECRET);
     req.user = decoded;
     next();
-  } catch {
+  } catch (err) {
     res.status(401).json({ error: "Invalid token" });
   }
 };
